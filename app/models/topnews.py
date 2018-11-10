5048c1a9480c6ef1510c0f91f51d048b7fb3f768
sir-meee <sammymbevi@gmail.com>
2018-11-10 18:00:28 +0300

    Defines topnews class

---
+++ app/models/topnews.py
@@ -0,0 +1,11 @@
+class Topnews:
+    """
+    topnews class to define topnews objects
+    """
+    def __init__(self,name,author,title,description,urlToImage,url):
+        self.name=name
+        self.author=author
+        self.title=title
+        self.description=description
+        self.urlToImage=urlToImage
+        self.url=url
