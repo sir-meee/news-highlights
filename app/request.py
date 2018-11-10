dd7d81f1d8507409b9d37f02fb885be2c735e61c
sir-meee <sammymbevi@gmail.com>
2018-11-10 18:02:39 +0300

    import modules to connect to api

--- app/request.py
+++ app/request.py
@@ -1,3 +1,11 @@
 from app import app
+import urllib.request,json
+from .models import topnews
+
+Topnews = topnews.Topnews
+ 
  # Getting api key
 api_key = app.config['NEWS_API_KEY']
+
+ # Getting top news base url
+base_url = app.config["TOPNEWS_API_BASE_URL"] 
