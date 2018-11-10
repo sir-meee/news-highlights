a3fdf4461c58753d2eb897c0ff910a1d828adf33
sir-meee <sammymbevi@gmail.com>
2018-11-10 18:50:33 +0300

    create function to get top news

--- app/request.py
+++ app/request.py
@@ -9,3 +9,16 @@ api_key = app.config['NEWS_API_KEY']
 
  # Getting top news base url
 base_url = app.config["TOPNEWS_API_BASE_URL"] 
+def get_topnews(source):
+    """
+    Function that gets the json response to our url request
+    """
+    get_topnews_url = base_url.format(source,api_key)
+    with urllib.request.urlopen(get_topnews_url) as url:
+        get_topnews_data = url.read()
+        get_topnews_response = json.loads(get_topnews_data)
+        topnews_results = None
+        if get_topnews_response['results']:
+            topnews_results_list = get_topnews_response['results']
+            topnews_results = process_results(topnews_results_list)
+    return topnews_results
