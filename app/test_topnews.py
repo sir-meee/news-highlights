114888d2dd424904845ef131590ed7ce99b0d00d
sir-meee <sammymbevi@gmail.com>
2018-11-10 17:57:46 +0300

    Add request file with api key

--- app/test_topnews.py
+++ app/test_topnews.py
@@ -1,16 +1,16 @@
 import unittest
 from models import topnews
-Topnews = topnews.Topnews
- class TopnewsTest(unittest.TestCase):
+Topnews = topnews.Topnews 
+class TopnewsTest(unittest.TestCase):
     '''
     Test Class to test the behaviour of the Topnews class
     '''
-     def setUp(self):
+    def setUp(self):
         '''
         Set up method that will run before every Test
         '''
         self.new_topnews = Topnews('Google News','Justin','NY times under fire','Pretty good','https://image.tmdb.org/t/p/w500/khsjha27hbs','www.foxnews.com')
-     def test_instance(self):
+    def test_instance(self):
         self.assertTrue(isinstance(self.new_topnews,Topnews))
- if __name__ == '__main__':
+if __name__ == '__main__':
     unittest.main()
