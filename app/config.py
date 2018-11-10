039e9eb09dbc806aed9fc9276ef9188cde4a46dc
sir-meee <sammymbevi@gmail.com>
2018-11-10 16:47:19 +0300

    Add top news api base url

--- app/config.py
+++ app/config.py
@@ -3,17 +3,18 @@ class Config:
     General configuration parent class
     '''
     pass
- class ProdConfig(Config):
+    TOPNEWS_API_BASE_URL ='https://newsapi.org/v2/top-headlines?sources={}&apiKey={}'
+class ProdConfig(Config):
     '''
     Production  configuration child class
      Args:
         Config: The parent configuration class with General configuration settings
     '''
     pass
- class DevConfig(Config):
+class DevConfig(Config):
     '''
     Development  configuration child class
      Args:
         Config: The parent configuration class with General configuration settings
     '''
-     DEBUG = True
+    DEBUG = True
