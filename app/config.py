db21d4a3431cb9830d29a6a9194cc60992d6e281
sir-meee <sammymbevi@gmail.com>
2018-11-11 19:08:53 +0300

    update config file and add api and secret key

--- app/config.py
+++ app/config.py
@@ -1,11 +1,14 @@
+import os
 class Config:
     '''
     General configuration parent class
     '''
-    pass
+    
     TOPNEWS_API_BASE_URL ='https://newsapi.org/v2/top-headlines?sources={}&apiKey={}'
     CATEGORIES_API_BASE_URL = 'https://newsapi.org/v2/sources?language=en&category={}&apiKey={}'
     ARTICLES_BASE_URL = 'https://newsapi.org/v2/everything?sources={}&apiKey={}'
+    NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
 class ProdConfig(Config):
     '''
     Production  configuration child class
@@ -20,3 +23,8 @@ class DevConfig(Config):
         Config: The parent configuration class with General configuration settings
     '''
     DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}    
