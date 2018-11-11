622f1075291006b358ee659738d17f94fe27a5ec
sir-meee <sammymbevi@gmail.com>
2018-11-11 06:21:23 +0300

    Gets google-news as source from API for top news

--- app/views.py
+++ app/views.py
@@ -6,5 +6,8 @@ def index():
      '''
     View root page function that returns the index page and its data
     '''
+     #Getting top news from google-news
+     top_articles = get_topnews('google-news')
+     print(top_news)
      title = 'Home -Get breaking news headlines, and search for articles from over 30,000 news sources and blogs'
-     return render_template('index.html', title = title)
+     return render_template('index.html', title = title,google_news= top_articles)
