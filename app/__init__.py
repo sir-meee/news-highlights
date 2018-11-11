a5228c4376729570c2bf6e656108712d4da43873
sir-meee <sammymbevi@gmail.com>
2018-11-11 21:57:00 +0300

    fix indentation errors

--- app/__init__.py
+++ app/__init__.py
@@ -1,18 +1,21 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
 from config import config_options
-#Initializing application
+# Initializing application
 
 bootstrap = Bootstrap()
 def create_app(config_name):
- 
-# Initializing Flask Extensions
-app = Flask(__name__)
-# Creating the app configurations
-app.config.from_object(config_options[config_name])
-# Initializing flask extensions
-bootstrap.init_app(app)
-# Registering the blueprint
-  from .main import main as main_blueprint
-  app.register_blueprint(main_blueprint)
-  return app 
+
+    app = Flask(__name__)
+
+    # Creating the app configurations
+    app.config.from_object(config_options[config_name])
+
+    # Initializing flask extensions
+    bootstrap.init_app(app)
+
+     # Registering the blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    return app 
